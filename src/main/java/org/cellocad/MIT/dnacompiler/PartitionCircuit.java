d641e4c015976e9cfdb54b03c2c221ce09b55f4c
jaipadmakumar <jaip217@gmail.com>
2017-08-15 15:18:57 -0400

    Fixed Combinations() method so can find all k combinations
    Fixed bug in Combinations() method that was preventing it from working
    when k!=2. Also, added a helper function to be used w/ this method.
    Code was essentially copied w/ light modification to work w/ List<Gate>
    instead of a int[] from an answer on stack overflow listed in
    documentation for the method.
    
    Also, removed original Combinations() method completely in favor of
    this one.

--- src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
+++ src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
@@ -360,35 +360,62 @@ public class PartitionCircuit {
 		return paths;
 	}
 	
-	/**Finds all 'k' length combinations of items in the list.
+	
+	/**
+	 * Iteratively finds all 'k' length combinations of gates in list. Algorithm uses the indexes 
+	 * of a list to generate all desired subsets of the list and then grabs the item from the 
+	 * original list using those indices. Code was copied (with slight modification) from the 
+	 * following answer on stackoverflow:
+	 * https://stackoverflow.com/a/29914908/3919605
 	 * 
-	 * @param arr list of Gates to find combinations of
-	 * @param k length of subsets to find. In other words, it's the 'k' in "n choose k"
-	 * @return list of k-item lists of gates representing all possible combinations of gates in {@code arr}
+	 * @param arr list of gates to find combinations of
+	 * @param k length of subsets to find. the 'k' in "n choose k"
+	 * @return list of k length sublists containing all possible combinations of k gates. 
 	 */
 	public static List<List<Gate>> Combinations(List<Gate> arr, int k){
-		//TODO Currently only works properly when k=2!
-		//TODO need to fix @ some point
-		
-		List<List<Gate>> combos = new ArrayList<List<Gate>>();
-		for(int i=0;i<arr.size();i++){
-			for(int j=i+k-1; j<arr.size(); j+=k-1){
-				//System.out.println("i= " + i + " j= " + j);
-				//List<Gate> subList = arr.subList(i, j);
-				List<Gate> subList = new ArrayList<Gate>();
-				//need a list expander to get all integers between i and j and index w/ those
-				//List<Integer> inds = Range(i, j);
-				//for(int p=0;p<inds.size();p++){subList.add(arr.get(inds.get(p)));};
-				subList.add(arr.get(i)); subList.add(arr.get(j));
-				combos.add(subList);
-			}
+		List<Gate> input = arr;    // input array
+
+		List<List<Gate>> subsets = new ArrayList<>();
+
+		int[] s = new int[k];                  // here we'll keep indices 
+		                                       // pointing to elements in input array
+
+		if (k <= input.size()) {
+		    // first index sequence: 0, 1, 2, ...
+		    for (int i = 0; (s[i] = i) < k - 1; i++);  
+		    	subsets.add(getSubset(input, s));
+		    for(;;) {
+		        int i;
+		        // find position of item that can be incremented
+		        for (i = k - 1; i >= 0 && s[i] == input.size() - k + i; i--); 
+		        if (i < 0) {
+		            break;
+		        }
+		        s[i]++;                    // increment this item
+		        for (++i; i < k; i++) {    // fill up remaining items
+		            s[i] = s[i - 1] + 1; 
+		        }
+		        subsets.add(getSubset(input, s));
+		    }
 		}
-		return combos;
+		return subsets;
+		
 	}
 	
-	//Gets all edges to cut, up to kth combinations of edges inclusive
-	//TODO Currently only returns single gates and k gate combinations
-	//		NOT ALL K COMBOS
+	
+	/** Helper function for findCombinations(). Generate actual subset by index sequence.
+	 * Original code copied from following answer on stackoverflow:
+	 * https://stackoverflow.com/a/29914908/3919605
+	 */
+	private static List<Gate> getSubset(List<Gate> input, int[] subset) {
+	    List<Gate> result = new ArrayList<Gate>(subset.length); 
+	    for(int i = 0; i < subset.length; i++) 
+	    	result.add(input.get(subset[i]));
+	    return result;
+	}
+
+	
+//Gets all edges to cut, up to kth combinations of edges inclusive
 	private List<List<Gate>> getValidEdges(int k){
 		List<List<Gate>> combos_list = new ArrayList<List<Gate>>();
 		for(Gate g: this.parent_lc.get_logic_gates()){
