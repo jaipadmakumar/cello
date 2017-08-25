1fe972aafce9ed396edb58a82619c13c68b9d7ac
jaipadmakumar <jaip217@gmail.com>
2017-08-25 12:48:27 -0400

    Added a class which will hold a partitioned circuit

---
+++ src/main/java/org/cellocad/MIT/dnacompiler/IntegratedLogicCircuit.java
@@ -0,0 +1,15 @@
+package org.cellocad.MIT.dnacompiler;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
+public class IntegratedLogicCircuit {
+	
+	LogicCircuit original_lc;
+	List<LogicCircuit> sub_lcs;
+	
+	
+
+}
