57150c170ff574d0ff5c969408b9f1539430fa55
jaipadmakumar <jaip217@gmail.com>
2017-08-25 12:50:02 -0400

    Commented lines out so doesn't crash

--- src/test/java/org/cellocad/MIT/dnacompiler/TestMain.java
+++ src/test/java/org/cellocad/MIT/dnacompiler/TestMain.java
@@ -1,14 +1,14 @@
-package org.cellocad.MIT.dnacompiler;
-
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
-
-public class TestMain {
-	 public static void main(String[] args) {
-		 Result result = JUnitCore.runClasses(PartitionCircuitTest.class);
-		 for (Failure failure : result.getFailures()) {
-		      System.out.println(failure.toString());
-		    }
-	 }
-}
+//package org.cellocad.MIT.dnacompiler;
+//
+//import org.junit.runner.JUnitCore;
+//import org.junit.runner.Result;
+//import org.junit.runner.notification.Failure;
+//
+//public class TestMain {
+//	 public static void main(String[] args) {
+//		 Result result = JUnitCore.runClasses(PartitionCircuitTest.class);
+//		 for (Failure failure : result.getFailures()) {
+//		      System.out.println(failure.toString());
+//		    }
+//	 }
+//}
