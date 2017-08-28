eb87724d3305090b2e03616fb125b28d92cba0a6
jaipadmakumar <jaip217@gmail.com>
2017-08-28 10:47:43 -0400

    lots of changes to make code functional... aka just a continuation of initial development

--- src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
+++ src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
@@ -83,12 +83,21 @@ public class PartitionCircuit {
 		}
 		
 		private void addPath(List<Gate> path_to_add){
+//			List<Gate> new_path_list = new ArrayList<Gate>();
+//			//make copies of gates
+//			for(Gate g: path_to_add) {
+//				Gate new_g = new Gate(g);
+//				new_path_list.add(new_g);
+//			}
 			paths.add(path_to_add);
 		}
 		
 		private void buildLogicCircuit(){
 			
 			//calculates subcircuit based on given subgraph paths
+			//GATES ALREADY HAVE WIRES ATTACHED FROM PARENT STRUCTURE, JUST USE THOSE IN CONSTRCUTOR 
+			//MIGHT NEED TO MAKE A COPY OF GATE BEFORE PUTTING INTO A SUBGRAPH SO THAT 
+			// DUPLICATE GATES IN DIFFERENT SUBGRAPHS ARE THEIR OWN OBJECTS IN MEMORY
 			HashSet<Gate> sublcs_gates = new HashSet<Gate>(); //lc gates set
 			HashSet<Wire> sublcs_wires = new HashSet<Wire>(); //lc wires set
 			int wire_count = 0;
@@ -135,7 +144,74 @@ public class PartitionCircuit {
 			}
 			LogicCircuit subcircuit = new LogicCircuit(new ArrayList<Gate>(sublcs_gates), 
 				 	new ArrayList<Wire>(sublcs_wires));
+			
 			this.sub_lc = subcircuit;
+		}
+		
+		private void buildLogicCircuit1(){
+
+			//calculates subcircuit based on given subgraph paths
+			//GATES ALREADY HAVE WIRES ATTACHED FROM PARENT STRUCTURE, JUST USE THOSE IN CONSTRCUTOR 
+			//MIGHT NEED TO MAKE A COPY OF GATE BEFORE PUTTING INTO A SUBGRAPH SO THAT 
+			// DUPLICATE GATES IN DIFFERENT SUBGRAPHS ARE THEIR OWN OBJECTS IN MEMORY
+			HashSet<Gate> sublcs_gates = new HashSet<Gate>(); //lc gates set
+			HashSet<Wire> sublcs_wires = new HashSet<Wire>(); //lc wires set
+			
+			
+			if(this.paths.size() == 1){
+				//only a single path through list so path is subgraph
+				//needs to be cast as ArrayList
+				
+				for(List<Gate> path:this.paths){
+					for(Gate g: path) { //make a gaet copy and add
+						Gate copy_g = new Gate(g);
+						sublcs_gates.add(copy_g);
+					}
+				}
+
+				for(Gate g:sublcs_gates){
+					//int gate_ind = g.Index;
+					//List<Integer> child_inds = new ArrayList<Integer>();
+					sublcs_wires.addAll(getGateWires(g));
+					}
+					
+			}
+			else{
+				//need to find set of gates from list
+				
+				List<Gate> gate_set = new ArrayList<Gate>(Union(this.paths));
+				for(Gate g: gate_set) { //make gate copy
+					Gate copy_g = new Gate(g);
+					sublcs_gates.add(copy_g);
+				}
+				
+				for(List<Gate> path:this.paths){
+					for(Gate g:path){
+						sublcs_wires.addAll(getGateWires(g));
+						}
+					}
+				}
+			
+			//System.out.println("subcircuit gates:\n" + sublcs_gates);
+			//System.out.println("subcircuit wires:\n" + sublcs_wires);
+			LogicCircuit subcircuit = new LogicCircuit(new ArrayList<Gate>(sublcs_gates), 
+				 	new ArrayList<Wire>(sublcs_wires));
+			
+			this.sub_lc = subcircuit;
+		}
+		private List<Wire> getGateWires(Gate g){
+			List<Wire> wires = new ArrayList<Wire>();
+			if(g.Type != Gate.GateType.INPUT) {
+				if(g.Outgoing != null) {
+					Wire wire = new Wire(g.Outgoing); //make copies
+					wires.add(wire);
+				}
+				if(g.Outgoing.Next != null) {
+					Wire wire = new Wire(g.Outgoing.Next);
+					wires.add(wire);
+					}
+				}
+			return wires;
 		}
 	}
 	
@@ -165,24 +241,15 @@ public class PartitionCircuit {
 	
 	//try testing w/ edge cuts:
 	//[6 NOT g4, 11 NOT g9]
+	//for iphone blue: [9 NOR g8, 7 NOR g6]
 	
 	public void partitionCircuit(LogicCircuit lc){
 		
 		List<Gate> empty_list = new ArrayList<Gate>();
 		List<Gate> lc_gates = lc.get_logic_gates();
 		//List<Gate> lc_gates = lc.get_Gates();
-		//System.out.println("logic gates \n" + lc_gates);
-		//Gate start = lc.get_input_gates().get(1);//lc_gates.get(0);
-		Gate end = lc.get_output_gates().get(0); //single output gate only currently
 		
-		//List<List<Gate>> test_paths = FindAllPaths(lc, end, start, empty_list );
-		//System.out.println("Start: " + start + " End: " + end);
-		//System.out.println("all paths: " + test_paths);
-		
-		//List<Gate> test_gate_combos = Arrays.asList(lc.get_logic_gates().get(1), lc.get_logic_gates().get(6),  lc.get_logic_gates().get(3), lc.get_logic_gates().get(2));
-		//List<Gate> test_gate_combos = Arrays.asList(lc_gates.get(3));
-		//List<Gate> test_gate_combos = Arrays.asList(lc_gates.get(2), lc_gates.get(8));
-		List<Gate> test_gate_combos = Arrays.asList(lc_gates.get(10), lc_gates.get(11));
+		List<Gate> test_gate_combos = Arrays.asList(lc_gates.get(7), lc_gates.get(5));
 		List<List<Gate>> gate_combinations = Arrays.asList(test_gate_combos);
 		
 		//since effectively cutting wires, might work to better explicitly do so
@@ -199,9 +266,10 @@ public class PartitionCircuit {
 		
 		List<List<Gate>> all_paths = new ArrayList<List<Gate>>();
 		for(Gate input_g:lc.get_input_gates()){
-			for(Gate output_g:lc.get_output_gates())
+			for(Gate output_g:lc.get_output_gates()) {
 				//System.out.println("Finding paths to gate: " + g);
 				all_paths.addAll(DepthFirstSearch.findAllPaths(lc, output_g, input_g, empty_list));
+			}
 		}
 		
 		//general algorithm:
@@ -226,7 +294,7 @@ public class PartitionCircuit {
 			partition_subgraph_map.put(combos_list, emptyGraphsList);
 			
 			//CALL METHOD HERE
-			HashMap<Gate, Subgraph> subgraph_path_map = SplitSingleEdge(lc, combos_list, all_paths);
+			HashMap<Gate, Subgraph> subgraph_path_map = SplitPathEdges(lc, combos_list, all_paths);
 			//System.out.println("Subgraph path map: " + subgraph_path_map);
 			//collect subgraphs from subgraph_path_map into single list and add to edge_partitions_dict
 			for(Gate g: subgraph_path_map.keySet()){
@@ -239,29 +307,62 @@ public class PartitionCircuit {
 		//use identified subgraphs to find logic subcircuits
 		List<List<LogicCircuit>> all_subgraphs_lc = new ArrayList<>();
 		System.out.println("Parent lc: \n" + this.parent_lc.printGraph());
+//		for(Gate g: this.parent_lc.get_Gates()) {
+//			System.out.println("Gate: " + g);
+//			if(g.Outgoing != null) {
+//				System.out.println("Wire: " + g.Outgoing);
+//			}
+//			if(g.Outgoing.Next != null) {
+//				System.out.println("Next Wire: " + g.Outgoing.Next);
+//			}
+//			System.out.println("////////////////////////////////////////////////////////");
+//			
+//		}
 		for(List<Gate >k:partition_subgraph_map.keySet()){
 			List<Subgraph> subgraphs = partition_subgraph_map.get(k);
 			List<LogicCircuit> subgraphs_lc = new ArrayList<>();
 			
 			
-			//following is just print statements for debugging except for one line that's important
+			
+			//following is just print statements for debugging except for two lines that are important
 			System.out.println("Edge Cut: " + k);
 			int sub_count = 1;
+			List<Subgraph> test_built_subgraphs = new ArrayList<Subgraph>();
 			for (Subgraph subgraph:subgraphs){
-				subgraph.buildLogicCircuit();
-				
+				subgraph.buildLogicCircuit1();
 				subgraphs_lc.add(subgraph.sub_lc);
+				Subgraph new_subgraph = new Subgraph(subgraph);
+				test_built_subgraphs.add(new_subgraph); //debugging
+				
 				//System.out.println("output gates" + this.parent_lc.get_output_gates());
 				System.out.println("output gate(s): " + subgraph.sub_lc.get_output_gates());
 				System.out.println("terminal gate: " + subgraph.terminal_gate);
 				System.out.println("terminal gate parents: " + subgraph.terminal_gate_parents);
-				System.out.println("Subgraph sub_lc " + sub_count + ":\n");
+				System.out.println("Subgraph sub_lc number: " + sub_count);
 				System.out.println("number of gates in sublc: " + subgraph.sub_lc.get_Gates().size());
+				System.out.println("Terminal gate children: " + subgraph.terminal_gate.getChildren());
 				System.out.println(subgraph.sub_lc.printGraph());
+				//printLogics(subgraph.sub_lc);
+			
 				sub_count +=1;
 			}
 			
+		///////////////////////debug block////////////////////////////
+			System.out.println("Determining integrated lc");
+				IntegratedLogicCircuit ic = new IntegratedLogicCircuit(this.parent_lc, test_built_subgraphs );
+				
+				//System.out.println(ic.sub_lcs);
+				for(LogicCircuit test_lc: ic.sub_lcs) {
+					System.out.println(test_lc.printGraph());
+				}
+				
+			///////////////////////end debug block////////////////////////////
+			
 			all_subgraphs_lc.add(subgraphs_lc);
+			
+//			//verify integrated circuit logics are equivalent to input logics
+//			IntegratedLogicCircuit ic = new IntegratedLogicCircuit(LogicCircuit parent_lc, subgraphs);
+			
 			
 
 		}
@@ -281,7 +382,7 @@ public class PartitionCircuit {
 	
 	//Helper method for partitionCircuit()
 	//Partitions a graph based on a single edge and returns a subgraph_path_map
-	public HashMap<Gate, Subgraph> SplitSingleEdge(LogicCircuit lc, List<Gate> edges_to_cut, List<List<Gate>> graph_paths){
+	public HashMap<Gate, Subgraph> SplitPathEdges(LogicCircuit lc, List<Gate> edges_to_cut, List<List<Gate>> graph_paths){
 		
 		//hashmap where Gate key = subgraph w/ terminal node = Gate
 		//then data validation aka deduping occurs w/in Subgraph class
@@ -295,7 +396,13 @@ public class PartitionCircuit {
 			subgraph_path_map.put(qs_gate, new Subgraph(qs_gate, getGateParents(this.parent_lc, qs_gate))); 
 		}
 		subgraph_path_map.put(lc.get_output_gates().get(0), new Subgraph());
-					
+		
+		
+		//THIS IS ONLY TRUE FOR CIRCUITS W/ A SINGLE OUTPUT TECHNICALLY
+		//may need to explicitly account for other terminal gates
+		// but output gates should only be at end of path so shouldn't matter
+		//b/c no actual slicing along w/ that gate is necessary? --> pretty sure it doesn't matter after 
+		//thinking more but double check in more detail
 		for(Gate qs_gate: edges_to_cut){
 			//have terminal node of current subgraph
 			
@@ -311,7 +418,8 @@ public class PartitionCircuit {
 			}
 			else{
 				//now build a subgraph terminating in qs node in terms of paths
-				//start with either an input gate or another quorum sensing gate
+				//start with either an input gate or another quorum sensing gate parent
+				
 		
 				for(List<Gate> full_path:graph_paths){
 					if(full_path.contains(qs_gate)){
@@ -324,14 +432,21 @@ public class PartitionCircuit {
 						//System.out.println("qs gate index: " + qs_gate_ind);
 						//System.out.println("full path: " + full_path);
 						List<Gate> subpath = full_path.subList(qs_gate_ind, terminal_ind);
-						System.out.println("subpath: " + subpath + "\n");
+						//System.out.println("subpath: " + subpath + "\n");
 						subgraph_path_map.get(qs_gate).addPath(subpath);
 					}
 					else{
+						//BUG: THIS PART LOOKS WEIRD --> IF qs_gate isn't on the path shouldn't need to do 
+						//any splitting. that being said, need to stop at ANY qs gate even if it's not the one 
+						//currently iterating on so maybe need to check if ANY of the edge_cut gates are 
+						//on the path
 						int terminal_ind = LookForward(edges_to_cut, qs_gate, full_path);
 						List<Gate> subpath = full_path.subList(0, terminal_ind);
+						//keys only go to single output but multiple outputs are possible
+						//shouldn't matter b/c complete paths run to same graph
 						subgraph_path_map.get(lc.get_output_gates().get(0)).addPath(subpath);
-						System.out.println("full path else: " + full_path);
+						//subgraph_path_map.get(lc.get_output_gates().get(0)).addPath(full_path);
+						//System.out.println("full path else: " + subpath);
 					}
 					
 				}
@@ -402,5 +517,31 @@ public class PartitionCircuit {
 		return bool;
 	}
 	
+	private void printLogics(LogicCircuit lc) {
+		for(Gate g:lc.get_Gates()) {
+			System.out.println(g + " logic: " + g.get_logics());
+		}
+	}
+	private List<List<Integer>> getInputLogics(LogicCircuit lc) {
+		List<List<Integer>> input_logics = new ArrayList<List<Integer>>();
+		for(Gate in: lc.get_input_gates()) {
+			input_logics.add(in.get_logics());
+		}
+		return input_logics;
+	}
+	
+	private List<List<Integer>> getOutputLogics(LogicCircuit lc) {
+		List<List<Integer>> output_logics = new ArrayList<List<Integer>>();
+		for(Gate out: lc.get_output_gates()) {
+			output_logics.add(out.get_logics());
+		}
+		return output_logics;
+	}
+	
+	
 
+	
+	
+	
+	
 }
