6dc7695d83620ca0c2b6bd0783a9e3401a007a73
jaipadmakumar <jaip217@gmail.com>
2017-09-01 14:24:44 -0400

    Changed graphviz integrated circuit output file names

--- src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
+++ src/main/java/org/cellocad/MIT/dnacompiler/PartitionCircuit.java
@@ -332,10 +332,13 @@ public class PartitionCircuit {
 				//System.out.println("terminal gate in pc ic call: " + test_built_subgraphs.get(1).terminal_gate);
 				Graphviz graphviz = new Graphviz(options.get_home(), options.get_output_directory(), options.get_jobID());
 				ScriptCommands script_commands = new ScriptCommands(options.get_home(), options.get_output_directory(), options.get_jobID());
-				graphviz.printGraphvizDotText(ic, "IC_WIRING_DIAGRAM.dot");
-		        script_commands.makeDot2Png("IC_WIRING_DIAGRAM.dot");
+				String outfile_name = "IC_WIRING_DIAGRAM_" + count;
+				for(Gate combo:ic.qs_gates) {outfile_name += "_" + combo.Name;}
+				outfile_name += ".dot";
+				graphviz.printGraphvizDotText(ic, outfile_name);
+		        script_commands.makeDot2Png(outfile_name);
 		        
-				
+		        count += 1;
 				//System.out.println(ic.sub_lcs);
 				for(LogicCircuit test_lc: ic.sub_lcs) {
 					System.out.println(test_lc.printGraph());
