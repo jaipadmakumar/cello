4266882b7cf35856de1aa869bb792ce71eed7ff6
jaipadmakumar <jaip217@gmail.com>
2017-09-11 12:54:17 -0400

    Finish algorithm development
    Fixed various issues to get a working function here. Also deleted
    extraneous comments and print statements. This function should now work
    properly and currently prints a topological ordering of the gates in
    the circuit.

--- src/main/java/org/cellocad/MIT/dnacompiler/TopologicalSort.java
+++ src/main/java/org/cellocad/MIT/dnacompiler/TopologicalSort.java
@@ -3,55 +3,56 @@ import java.io.*;
 import java.util.*;
 
 
+
+
+
 public class TopologicalSort {
 	
-	
+	/*
+	 * Helper function for topoSort. Basically runs a depth first search through graph (starting at an output node
+	 * b/c that's what method is called with initially) and pushes a gate to the stack if it:
+	 * 	a) goes down a path where it doesn't encounter any visited nodes and hits an input gate (no children). in this 
+	 * 	   case, it backtracks through the path, pushing gates to the stack as it backtracks in same as order as
+	 * 	   they're visited in the dfs. or,  
+	 *	b) goes down a path where it visits an already seen node, in which case it backtracks at that node and pushes
+	 *	   parent of seen gate into stack. This ensures the topological ordering.  
+	 */
 	private static void topoSortHelper(Gate g, List<Gate> visited, Deque<Gate> stack) {
 		
-		Gate next_g;
-		g.set_unvisited(false); //mark current gate as visited
-		System.out.println("Current gate: " + g);
+		Gate next_g; //child gate to recurse w/
+		visited.add(g); //mark current gate as visited
+		
 		ListIterator<Gate> child_iter = g.getChildren().listIterator();
+		
+		//loop is entered as long as a gate has a child aka as long as its not an input gate
 		while(child_iter.hasNext()) {
-			next_g = child_iter.next();
-			System.out.println("next gate: " + next_g);
-			if(next_g.is_unvisited()) {
+			next_g = child_iter.next(); //recursion tree splits here if gate has >1 child
+			if(!visited.contains(next_g)) { //if gate is unvisited, recurse b/c need to keep going down path, else backtrack
+				//System.out.println("visiting gate: " + next_g);
 				topoSortHelper(next_g, visited, stack);
 			}
 		}
-		stack.add(g);
+		
+		//get to here when gate has no children aka at end of path
+		//OR if encountered an already seen gate, in which case backtrack and push original parent gate
+		
+		stack.push(g);
 		
 		
 	}
 	
-	//will need to add wires to input nodes in lc connecting to super source
-	// and then remove them at end
-	//will also want to clear all g.unvisited()
+
 	public static void topoSort(LogicCircuit lc) {
-		//Stack<Gate> stack = new Stack<Gate>();
-		//stack.addAll(old_stack);
 		System.out.println("Calculating topological sort");
 		Deque<Gate> stack = new ArrayDeque<Gate>();
 		List<Gate> visited_gates = new ArrayList<Gate>();
 		
-		
-		
-		//initialize everything to unvisited
-		for(Gate g: lc.get_Gates()) {
-			g.set_unvisited(true);
+		//initiate depth first search starting from output gates, only one stack and visited_gates array
+		//in memory so will be appropriately filled out by helper function
+		for(Gate out_g: lc.get_output_gates()) {
+			topoSortHelper(out_g, visited_gates, stack);
 		}
-		for(Gate g:lc.get_input_gates()) {g.set_unvisited(false);}
-		
-		for(Gate g:lc.get_Gates()) {
-			System.out.println("current stack: " + stack);
-			System.out.println("stack length: " + stack.size());
-			if(g.is_unvisited()) {
-				topoSortHelper(g, visited_gates, stack);
-				
-			}
-			
-		}
-		
+		System.out.println("toplogical order:");
 		while (stack.isEmpty()==false) {
 			System.out.println(stack.pop() + " ");
 		}
